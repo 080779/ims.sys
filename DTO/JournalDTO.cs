e7c282d624d1743e55289e8d08f406b6b01a1848
vz深渊 <exceed295@hotmail.com>
2018-05-04 00:06:49 +0800

    45

---
+++ DTO/JournalDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.DTO
+{
+    public class JournalDTO:BaseDTO
+    {
+        public long PlatformUserId { get; set; }
+        public long ToPlatformUserId { get; set; }
+        public long JournalTypeId { get; set; }
+        public long ChangeTypeId { get; set; }
+        public long IntegralTypeId { get; set; }
+        public long StateId { get; set; }
+        public string Description { get; set; }
+        public decimal? InAmount { get; set; }
+        public decimal? OutAmount { get; set; }
+        public decimal? Amount { get; set; }
+        public string JournalTypeName { get; set; }
+        public string PlatformUserMobile { get; set; }
+        public string ToPlatformUserMobile { get; set; }
+        public string ChangeTypeName { get; set; }
+        public string StateName { get; set; }
+    }
+}
--- DTO/PlatformUserDTO.cs
+++ DTO/PlatformUserDTO.cs
@@ -4,26 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IMS.Service.Entity
+namespace IMS.DTO
 {
-    /// <summary>
-    /// 平台实体类
-    /// </summary>
-    public class PlatformUserEntity : BaseEntity
+    public class PlatformUserDTO:BaseDTO
     {
         public string Mobile { get; set; }
         public string UserCode { get; set; }
         public string Description { get; set; }
         public decimal GivingAmount { get; set; }
-        public decimal UseAmount { get; set; } = 0;
+        public decimal UseAmount { get; set; }
         public string Salt { get; set; }
         public string Password { get; set; }
-        public int ErrorCount { get; set; } = 0;
-        public DateTime ErrorTime { get; set; } = DateTime.Now;
+        public int ErrorCount { get; set; }
+        public DateTime ErrorTime { get; set; }
         public bool IsEnabled { get; set; }
         public long PlatformUserTypeId { get; set; }
         public long IntegralTypeId { get; set; }
-        public virtual PlatformUserTypeEntity PlatformUserType { get; set; }
-        public virtual IntegralTypeEntity IntegralType { get; set; }
+        public string  PlatformUserTypeName { get; set; }
+        public  string IntegralTypeName { get; set; }
     }
 }
